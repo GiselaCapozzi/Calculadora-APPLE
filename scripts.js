534500c245f9a9271414cfcf218ec3a4db7937d0
GiselaCapozzi <gisecapozzi@gmail.com>
2021-11-02 20:52:32 -0300

    Capturando el valor del click de los botones y creando la funcion de filtro

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function getContentClick(event) {
+  const value = event.target.innerHTML;
+  filterAction(value)
+};
+
+const filterAction = (value) => {
+  console.log('Estamos en filterAction');
+  console.log(value);
+}
