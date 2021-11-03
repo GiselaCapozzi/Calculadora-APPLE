6fe97a3cd74d4929bb63345e6623302a12089f59
GiselaCapozzi <gisecapozzi@gmail.com>
2021-11-02 21:22:29 -0300

    Cambiando el valor de la screen al presionar algún número

--- scripts.js
+++ scripts.js
@@ -4,6 +4,26 @@ function getContentClick(event) {
 };
 
 const filterAction = (value) => {
-  console.log('Estamos en filterAction');
-  console.log(value);
+  value === '0' ? addNumberInput(0) : null;
+  value === '1' ? addNumberInput(1) : null;
+  value === '2' ? addNumberInput(2) : null;
+  value === '3' ? addNumberInput(3) : null;
+  value === '4' ? addNumberInput(4) : null;
+  value === '5' ? addNumberInput(5) : null;
+  value === '6' ? addNumberInput(6) : null;
+  value === '7' ? addNumberInput(7) : null;
+  value === '8' ? addNumberInput(8) : null;
+  value === '9' ? addNumberInput(9) : null;
+  value === ',' ? addNumberInput(',') : null;
+}
+
+function addNumberInput(value) {
+  const inputScreen = document.getElementsByClassName('calculator__screen')[0];
+  const inputValue = inputScreen.value;
+
+  if (inputValue === '0' && inputValue.length === 1 && value !== ',') {
+    inputScreen.value = value;
+    return;
+  }
+  inputScreen.value = inputValue + value;
 }
